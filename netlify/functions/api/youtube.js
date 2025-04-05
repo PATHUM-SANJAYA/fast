7578afa53ad6f22f55adf20dc56eabf356db1ee1
PATHUM-SANJAYA <ishankaanjalitechtack@gmail.com>
2025-04-05 14:16:54 +0530

    Fix function deployment configuration

---
+++ netlify/functions/api/youtube.js
@@ -0,0 +1,52 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const ytdl_core_1 = __importDefault(require("ytdl-core"));
+const handler = async (event) => {
+    var _a;
+    if (event.httpMethod !== 'GET') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ error: 'Method not allowed' })
+        };
+    }
+    const videoUrl = (_a = event.queryStringParameters) === null || _a === void 0 ? void 0 : _a.url;
+    if (!videoUrl) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'URL parameter is required' })
+        };
+    }
+    try {
+        const info = await ytdl_core_1.default.getInfo(videoUrl);
+        const videoInfo = {
+            title: info.videoDetails.title,
+            author: info.videoDetails.author.name,
+            lengthSeconds: info.videoDetails.lengthSeconds,
+            viewCount: info.videoDetails.viewCount,
+            formats: info.formats.map(format => ({
+                itag: format.itag,
+                url: format.url,
+                mimeType: format.mimeType,
+                quality: String(format.quality),
+                qualityLabel: format.qualityLabel,
+                contentLength: format.contentLength
+            }))
+        };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(videoInfo)
+        };
+    }
+    catch (error) {
+        console.error('Error fetching video info:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to fetch video information' })
+        };
+    }
+};
+exports.handler = handler;
